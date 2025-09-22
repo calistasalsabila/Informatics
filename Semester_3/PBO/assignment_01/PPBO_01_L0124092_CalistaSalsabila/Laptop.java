a4617e9d952435be6a78fafe7d356efc26fd8188
calista <calistasalsabila313@gmail.com>
2025-09-22 08:20:18 +0700

    update

---
+++ Semester_3/PBO/assignment_01/PPBO_01_L0124092_CalistaSalsabila/Laptop.java
@@ -0,0 +1,33 @@
+// Nama : Calista Salsabila
+// NIM  : L0124092
+
+// package
+package PPBO_01_L0124092_CalistaSalsabila;
+
+// class
+public class Laptop {
+
+    // constructor
+    String brand, dgpu;
+    int ram, storage;
+
+    Laptop(String brand, String dgpu, int ram, int storage){
+        this.brand = brand;
+        this.dgpu = dgpu;
+        this.ram = ram;
+        this.storage = storage;
+    }
+
+    public void displayInfo(){
+        System.out.println("Nama laptop: " +brand);
+        System.out.println("GPU Laptop: " +dgpu);
+        System.out.println("RAM laptop: " +ram);
+        System.out.println("Storage: " +storage);
+    }
+
+
+
+
+}
+
+
---
+++ Semester_3/PBO/assignment_01/PPBO_01_L0124092_CalistaSalsabila/PPBO_01_L0124092_CALISTASALSABILA.java
@@ -0,0 +1,16 @@
+// Nama : Calista Salsabila
+// NIM  : L0124092
+
+// package
+package PPBO_01_L0124092_CalistaSalsabila;
+
+// class
+public class PPBO_01_L0124092_CALISTASALSABILA {
+    public static void main(String[] args) {
+
+        // object
+        Laptop laptop1 = new Laptop("HP OMEN", "RTX 4070", 32, 1000);
+
+        laptop1.displayInfo();
+    }
+}
