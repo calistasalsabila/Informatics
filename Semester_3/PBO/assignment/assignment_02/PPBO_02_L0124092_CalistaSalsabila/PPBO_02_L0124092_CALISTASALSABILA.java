81aa106f1fff8068ebb82f532a871fa1627b7630
calista <calistasalsabila313@gmail.com>
2025-09-25 14:37:45 +0700

    update

--- Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/PPBO_02_L0124092_CALISTASALSABILA.java
+++ Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/PPBO_02_L0124092_CALISTASALSABILA.java
@@ -6,9 +6,9 @@ public class PPBO_02_L0124092_CALISTASALSABILA {
     
     public static void main(String[] args) {
         System.out.println("---");
-        System.out.println("1. Pakai Barang");
-        System.out.println("2. Cek Stock");
-        System.out.println("3. Tambah Stock");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
         
     }
 }
--- Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/controller/BarangContorller.java
+++ Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/controller/BarangContorller.java
@@ -1,5 +1,5 @@
 package Semester_3.PBO.assignment.assignment_02.PPBO_02_L0124092_CalistaSalsabila.controller;
 
-public class Controller {
+public class BarangContorller {
     
 }
