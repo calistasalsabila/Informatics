6b8ff87014ee74aade67a27044c04e5f25c4d383
calista <calistasalsabila313@gmail.com>
2025-09-25 13:49:25 +0700

    add asign 2

---
+++ Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/PPBO_02_L0124092_CALISTASALSABILA.java
@@ -0,0 +1,14 @@
+package Semester_3.PBO.assignment.assignment_02.PPBO_02_L0124092_CalistaSalsabila;
+
+import java.util.Scanner;
+
+public class PPBO_02_L0124092_CALISTASALSABILA {
+    
+    public static void main(String[] args) {
+        System.out.println("---");
+        System.out.println("1. Pakai Barang");
+        System.out.println("2. Cek Stock");
+        System.out.println("3. Tambah Stock");
+        
+    }
+}
