9e2c3211920e97cafac9be4c4d8984fb082acd7b
calista <calistasalsabila313@gmail.com>
2025-09-25 17:37:07 +0700

    create inventaris class

--- Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/controller/InventarisController.java
+++ Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/controller/InventarisController.java
@@ -1,5 +1,10 @@
 package Semester_3.PBO.assignment.assignment_02.PPBO_02_L0124092_CalistaSalsabila.controller;
 
+import java.util.ArrayList;
+import model.Barang;
+import model.Inventaris;
+
+
 public class InventarisController {
     
 }
--- Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/model/Inventaris.java
+++ Semester_3/PBO/assignment/assignment_02/PPBO_02_L0124092_CalistaSalsabila/model/Inventaris.java
@@ -1,6 +1,20 @@
 package Semester_3.PBO.assignment.assignment_02.PPBO_02_L0124092_CalistaSalsabila.model;
 
-
+import java.util.ArrayList;
 public class Inventaris {
+
+    private ArrayLIst<Barang> daftarBarang;
+
+    public Inventaris(){
+        daftarBarang = new ArrayList<>();
+    }
+
+    public ArrayLIst<Barang> getDaftarBarang() {
+        return daftarBarang;
+    }
+
+    public void setDaftarBarang(ArrayLIst<Barang> daftarBarang) {
+        this.daftarBarang = daftarBarang;
+    }
     
 }
