2aeccf4dd8dcbe42d9d5c4c0baa7b1cdaa2553e5
calista <calistasalsabila313@gmail.com>
2025-10-13 10:48:43 +0700

    update

--- Semester_3/PBO/assignment/assignment_04/PPBO_04_L0124092_CALISTASALSABILA/PPBO_04_L0124092.java
+++ Semester_3/PBO/assignment/assignment_04/PPBO_04_L0124092_CALISTASALSABILA/PPBO_04_L0124092.java
@@ -1,6 +1,5 @@
 package Semester_3.PBO.assignment.assignment_04.PPBO_04_L0124092_CALISTASALSABILA;
 
-import Semester_3.PBO.assignment.assignment_02_play.mvc.main;
 
 public class PPBO_04_L0124092 {
     public static void main(String[] args) {
