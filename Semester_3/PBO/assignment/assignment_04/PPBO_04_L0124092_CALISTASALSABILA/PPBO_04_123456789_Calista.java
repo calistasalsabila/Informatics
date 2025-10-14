95333f56fbf43f0b4f07cbd7e02d322f4e03fee3
calista <calistasalsabila313@gmail.com>
2025-10-14 15:36:03 +0700

    without try catch

--- Semester_3/PBO/assignment/assignment_04/PPBO_04_L0124092_CALISTASALSABILA/PPBO_04_123456789_Calista.java
+++ Semester_3/PBO/assignment/assignment_04/PPBO_04_L0124092_CALISTASALSABILA/PPBO_04_123456789_Calista.java
@@ -1,80 +1,46 @@
-// Nama : Calista
-// NIM  : 123456789
-
 package Semester_3.PBO.assignment.assignment_04.PPBO_04_L0124092_CALISTASALSABILA;
 
 import java.util.*;
 
-class Barang {
-    String nama;
-    double harga;
-    int stok;
-
-    Barang(String nama, double harga, int stok) {
-        this.nama = nama;
-        this.harga = harga;
-        this.stok = stok;
-    }
-
-    void kurangiStok(int jumlah) throws Exception {
-        if (jumlah > stok) {
-            throw new Exception("stok tidak cukup untuk " + nama);
-        }
-        stok -= jumlah;
-    }
-
-    @Override
-    public String toString() {
-        return nama + " - Rp" + harga + " (Stok: " + stok + ")";
-    }
-}
-
 public class PPBO_04_123456789_Calista {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        List<Barang> daftarBarang = new ArrayList<>();
-        daftarBarang.add(new Barang("Pensil", 2000, 10));
-        daftarBarang.add(new Barang("Buku", 5000, 5));
-        daftarBarang.add(new Barang("Penghapus", 1500, 8));
+        Barang barang = new Barang("Unbreakable Faith", 1, 1);
+        Barang barang1 = new Barang("Ways of Survival", 999999999, 3);
+        Barang barang2 = new Barang("4th Wall", 1, 1);
+        Barang barang3 = new Barang("Break Heavenly Demon Sword", 1, 1);
 
-        System.out.println("Daftar Barang:");
 
-        for (int i = 0; i < daftarBarang.size(); i++) {
-            System.out.println((i + 1) + ". " + daftarBarang.get(i));
+        while (true) {
+            System.out.println("Welcome to ORV Store");
+            System.out.println("well, u can just buy one object hehe");
+            System.out.println("1. Tampilkan Barang");
+            System.out.println("2. Beli n Bayar");
+            System.out.println("3. Keluar");
+            System.out.print("? : ");
+
+            int opsi = sc.nextInt();
+            sc.nextLine();
+
+            switch (opsi) {
+                case 1:
+                    barang.tampilkanBarang();
+                    break;
+                case 2:
+                    System.out.print("Masukkan nama barang: ");
+                    String namaBarang = sc.nextLine();
+                    System.out.print("jumlah: ");
+                    int jumlahBeli = sc.nextInt();
+                    sc.nextLine();
+                    barang.beli(namaBarang, jumlahBeli, sc);
+                    break;
+                case 3:
+                    System.out.println("ok");
+                    return; 
+                default:
+                    System.out.println("Pilihan tidak valid, coba lagi.");
+            }
         }
-
-        try {
-            System.out.print("\nMasukkan nomor barang yang ingin dibeli: ");
-            int pilihan = input.nextInt();
-
-            assert (pilihan > 0 && pilihan <= daftarBarang.size()) : "Pilihan tidak valid!";
-
-            Barang barangDipilih = daftarBarang.get(pilihan - 1);
-            System.out.print("Masukkan jumlah yang ingin dibeli: ");
-            int jumlah = input.nextInt();
-
-            assert (jumlah > 0) : "Jumlah pembelian harus lebih dari 0!";
-
-            barangDipilih.kurangiStok(jumlah);
-            double total = jumlah * barangDipilih.harga;
-
-            System.out.println("\nembelian berhasil!");
-            System.out.println("Barang: " + barangDipilih.nama);
-            System.out.println("Jumlah: " + jumlah);
-            System.out.println("Total Bayar: Rp" + total);
-
-        } catch (AssertionError ae) {
-            System.out.println(ae.getMessage());
-        } catch (InputMismatchException ime) {
-            System.out.println("Input harus berupa angka!");
-        } catch (Exception e) {
-            System.out.println("Terjadi kesalahan: " + e.getMessage());
-        } finally {
-            System.out.println("Ty");
-        }
-
-        input.close();
     }
 }
-
