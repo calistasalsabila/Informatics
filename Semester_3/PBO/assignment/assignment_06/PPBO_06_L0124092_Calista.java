6567c235c6c2a7f228a5c903d8088f4d61a09bee
calista <calistasalsabila313@gmail.com>
2025-10-31 14:37:24 +0700

    update

---
+++ Semester_3/PBO/assignment/assignment_06/PPBO_06_L0124092_Calista.java
@@ -0,0 +1,88 @@
+package PBO.assignment.assignment_06;
+
+// Nama : Calista
+// NIM  : L0124092
+
+import java.util.*;
+
+public class PPBO_06_L0124092_Calista {
+    public static void main(String[] args) {
+       
+        String[] nimArr = {"231001", "231002", "231003"};
+        String[] namaArr = {"Dokja", "Shirone", "Cale"};
+        int[] nilaiArr = {85, 90, 78};
+
+        for (int i = 0; i < nimArr.length; i++) {
+            System.out.println(nimArr[i] + " - " + namaArr[i] + " - " + nilaiArr[i]);
+        }
+
+        class Mahasiswa {
+            String nim;
+            String nama;
+            int nilai;
+
+            Mahasiswa(String nim, String nama, int nilai) {
+                this.nim = nim;
+                this.nama = nama;
+                this.nilai = nilai;
+            }
+
+            public String toString() {
+                return nim + " - " + nama + " - " + nilai;
+            }
+        }
+
+        ArrayList<Mahasiswa> listMhs = new ArrayList<>();
+
+        listMhs.add(new Mahasiswa("231004", "Jeha", 88));
+        listMhs.add(new Mahasiswa("231005", "Eruki", 92));
+        listMhs.add(new Mahasiswa("231006", "Neid", 75));
+
+        System.out.println("Data awal:");
+        for (Mahasiswa m : listMhs) {
+            System.out.println(m);
+        }
+
+        String nimHapus = "231005";
+        listMhs.removeIf(m -> m.nim.equals(nimHapus));
+        System.out.println("\nSetelah menghapus NIM " + nimHapus + ":");
+        for (Mahasiswa m : listMhs) {
+            System.out.println(m);
+        }
+
+        HashSet<String> namaUnik = new HashSet<>();
+        for (Mahasiswa m : listMhs) {
+            namaUnik.add(m.nama);
+        }
+        for (String nama : namaUnik) {
+            System.out.println(nama);
+        }
+
+      
+        HashMap<String, Integer> mapNilai = new HashMap<>();
+        mapNilai.put("231004", 88);
+        mapNilai.put("231006", 75);
+        mapNilai.put("231007", 95);
+
+        for (String nim : mapNilai.keySet()) {
+            System.out.println(nim + " : " + mapNilai.get(nim));
+        }
+
+        double total = 0;
+        for (int nilai : mapNilai.values()) {
+            total += nilai;
+        }
+        double rata = total / mapNilai.size();
+        System.out.println("Rata-rata nilai = " + rata);
+
+        Collections.sort(listMhs, Comparator.comparingInt(m -> m.nilai));
+        for (Mahasiswa m : listMhs) {
+            System.out.println(m);
+        }
+
+        Collections.sort(listMhs, Comparator.comparing(m -> m.nama));
+        for (Mahasiswa m : listMhs) {
+            System.out.println(m);
+        }
+    }
+}
