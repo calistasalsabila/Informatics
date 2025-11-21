b592692ccbd541d8ae587b4bd06aad75de00c714
calista <calistasalsabila313@gmail.com>
2025-11-21 19:08:00 +0700

    delete setter from Item file

---
+++ Semester_3/PBO/Responsi/src/model/GameCharacter.java
@@ -0,0 +1,11 @@
+package model;
+
+public abstract class GameCharacter {
+    protected String name;
+    protected int maxHp;
+    protected int hp;
+    protected int baseAttack;
+
+
+    
+}
--- Semester_3/PBO/Responsi/src/model/Item.java
+++ Semester_3/PBO/Responsi/src/model/Item.java
@@ -15,29 +15,14 @@ public class Item {
         return name;
     }
 
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
     public String getType() {
         return type;
     }
-
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
 
     public int getValue() {
         return value;
     }
 
-
-    public void setValue(int value) {
-        this.value = value;
-    }
+   
     
 }
