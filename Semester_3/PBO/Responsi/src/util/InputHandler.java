972325f899666320e01630afa450426a46f82d86
calista <calistasalsabila313@gmail.com>
2025-11-21 20:48:33 +0700

    add InputHandler

--- Semester_3/PBO/Responsi/src/util/InputHandler.java
+++ Semester_3/PBO/Responsi/src/util/InputHandler.java
@@ -1,5 +1,25 @@
 package util;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class InputHandler {
-    
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public int getInt(String prompt) {
+        System.out.print(prompt + " >> ");
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("error: masukin angka");
+            scanner.nextLine();
+            return -1;
+        }
+    }
+
+    public String getString(String prompt) {
+        System.out.print(prompt + " >> ");
+        return scanner.next();
+    }
 }
